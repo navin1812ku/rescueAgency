c1eedfb7621f1c3e9eeeb58c1b7fee28f41f37f9
navin1812ku <navin1812ku@gmail.com>
2024-02-09 19:47:13 +0530

    first commit

---
+++ src/main/java/rescue/agency/RescueAgency/RescueAgencyApplication.java
@@ -0,0 +1,13 @@
+package rescue.agency.RescueAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescueAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rescue/agency/RescueAgency/RescueAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package rescue.agency.RescueAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
